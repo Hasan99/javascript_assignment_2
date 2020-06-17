4d47c695f9453f71ab68bd5a372d8f224bc64a83
Hasan Mohiuddin Ahmed <hasan_libra95@hotmail.com>
2020-06-18 03:45:26 +0500

    completed chapter 26 to 30

---
+++ chapter26_to_30/app.js
@@ -0,0 +1,77 @@
+// Question 1
+// var number = Number(prompt("Enter number"));
+// document.write(`Number: ${number}<br />`);
+// document.write(`Round off value: ${Math.round(number)}<br />`);
+// document.write(`Floor value: ${Math.floor(number)}<br />`);
+// document.write(`Ceil value: ${Math.ceil(number)}`);
+
+
+
+// Question 2
+// var number = Number(prompt("Enter number"));
+// document.write(`Number: ${number}<br />`);
+// document.write(`Round off value: ${Math.round(number)}<br />`);
+// document.write(`Floor value: ${Math.floor(number)}<br />`);
+// document.write(`Ceil value: ${Math.ceil(number)}`);
+
+
+
+// Question 3
+// var number = -4;
+// document.write(`The absolute value of ${number} is ${Math.abs(number)}`);
+
+
+
+// Question 4
+// var dice = Math.random();
+// dice = (dice * 6) + 1;
+// dice = Math.floor(dice);
+// document.write(`Random dice value: ${dice}`);
+
+
+
+// Question 5
+// var coin = Math.random();
+// coin = (coin * 2) + 1;
+// coin = Math.floor(coin);
+// var result = "";
+// if (coin === 1) {
+//     result = "Tails";
+// }
+// else {
+//     result = "Heads";
+// }
+// document.write(`${coin}<br />`);
+// document.write(`Random coin value: ${result}`);
+
+
+
+// Question 6
+// var number = Math.random();
+// number = (random * 100) + 1;
+// number = Math.floor(number);
+// document.write(`Random number between 1 and 100: ${number}`);
+
+
+
+// Question 7
+// var weight = prompt("Enter your weight in kilograms");
+// weight = weight.replace("kgs", "");
+// weight = weight.replace("kg", "");
+// weight = weight.replace("kilograms", "");
+// weight = weight.replace("kilogram", "");
+// document.write(`The weight of user is ${weight} kilograms`);
+
+
+
+// Question 8
+// var number = Math.random();
+// number = (number * 10) + 1;
+// number = Math.floor(number);
+// var input = parseInt(prompt("Enter a number between 1 and 10"));
+// if (input === number) {
+//     alert("Congratulations :)")
+// }
+// else {
+//     alert("Try Again!");
+// }
